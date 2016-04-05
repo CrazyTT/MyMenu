fef838617281348f8d7b726c950014a0c900b0f0
陈柳柳 <chenliuliu123@126.com>
2016-04-05 16:01:42 +0800

    更新

--- app/src/main/java/com/chenliuliu/mymenu/MainActivity.java
+++ app/src/main/java/com/chenliuliu/mymenu/MainActivity.java
@@ -32,37 +32,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-
         mBtnMeun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!isOpen) {
-                    isOpen = true;
-                    ViewAnimator.animate(mBtnOne).dp().translationY(0, -200).duration(600).translationX(0, -100)
-                            .andAnimate(mBtnTwo).dp().translationY(0, -100).translationX(0, -150)
-                            .andAnimate(mBtnThree).dp().translationY(0, -100)
-                            .andAnimate(mBtnFour).dp().translationY(0, -200)
-                            .andAnimate(mBtnFive).dp().translationX(0, -200)
-                            .andAnimate(mBtnMeun).rotation(45)
-                            .thenAnimate(mBtnOne, mBtnTwo, mBtnThree, mBtnFour, mBtnFive).tada().wave()
-                            .descelerate().start();
+                    openMenu();
                 } else {
-                    isOpen = false;
-                    ViewAnimator.animate(mBtnOne).dp().translationY(-200, 0).duration(600).translationX(-100, 0)
-                            .andAnimate(mBtnTwo).dp().translationY(-100, 0).translationX(-150, 0)
-                            .andAnimate(mBtnThree).dp().translationY(-100, 0)
-                            .andAnimate(mBtnFour).dp().translationY(-200, 0)
-                            .andAnimate(mBtnFive).dp().translationX(-200, 0)
-                            .andAnimate(mBtnMeun).rotation(0).shake()
-                            .accelerate().start();
-
+                    closeMenu();
                 }
             }
         });
-
-
     }
 
+    private void openMenu() {
+        isOpen = true;
+        ViewAnimator.animate(mBtnOne).dp().translationY(0, -200).duration(600).translationX(0, -100)
+                .andAnimate(mBtnTwo).dp().translationY(0, -100).translationX(0, -150)
+                .andAnimate(mBtnThree).dp().translationY(0, -100)
+                .andAnimate(mBtnFour).dp().translationY(0, -200)
+                .andAnimate(mBtnFive).dp().translationX(0, -200)
+                .andAnimate(mBtnMeun).rotation(45)
+                .thenAnimate(mBtnOne, mBtnTwo, mBtnThree, mBtnFour, mBtnFive).tada().wave()
+                .descelerate().start();
+    }
+
+    private void closeMenu() {
+        isOpen = false;
+        ViewAnimator.animate(mBtnOne).dp().translationY(-200, 0).duration(600).translationX(-100, 0)
+                .andAnimate(mBtnTwo).dp().translationY(-100, 0).translationX(-150, 0)
+                .andAnimate(mBtnThree).dp().translationY(-100, 0)
+                .andAnimate(mBtnFour).dp().translationY(-200, 0)
+                .andAnimate(mBtnFive).dp().translationX(-200, 0)
+                .andAnimate(mBtnMeun).rotation(0).shake()
+                .accelerate().start();
+
+    }
 
 }
